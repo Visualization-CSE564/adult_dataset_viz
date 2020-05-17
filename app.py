90bcf78eadd95c20537934d63dd06f7c03d72876
Paridhi Somani <pasomani@cs.stonybrook.edu>
2020-05-17 04:27:43 -0400

    test commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
-    return render_template("index.html", data = {'text': 'Visualization Project'})
+    return render_template("test.html", data = {'text': 'Visualization Project'})
 
 @app.route("/pc", methods = ['POST', 'GET'])
 def data_load_pc():
@@ -16,13 +16,16 @@ def data_load_pc():
     data_df = dataframe.filter(['index','age','fnlwgt','capital_gain','capital_loss','hours_per_week'], axis=1)
     return {"data_dict" : data_df.values.tolist()}
 
-# @app.route("/piechart", methods = ['POST'])
-# def pie_data():
-#     global dataframe
-#     data_df = dataframe.filter(['index','race','sex','Income'],axis=1)
-
-#     if request.form['list'] == None:
-        
+@app.route("/piechart", methods = ['POST'])
+def pie_data():
+    global dataframe
+    data_df = dataframe.filter(['race','income','index'],axis=1)
+    print(dataframe.columns)
+    # if request.form['list'] == None:
+    filter_df = data_df.groupby(by = ['race','income'])
+    filter_df = filter_df.count()
+    filter_df.reset_index(inplace=True) 
+    return {"data_dict" : filter_df.values.tolist()}
 
 
 def load_data():
