fbef5e97b5cbcc8bfe361e421ea026cd18f1706b
Bishal Kumar Shrestha <bishalsht@gmail.com>
2020-05-18 15:17:34 -0400

    sync checkpt

--- static/scripts/custom_draw.js
+++ static/scripts/custom_draw.js
@@ -1,8 +1,10 @@
 var svg_position = {
-    row1: {height: 130, width1: 1200},
+    row1: {height: 120, width1: 1200},
     row2: {height: 400, width1: 750, width2: 450},
     row3: {height: 250, width1: 450, width2: 400, width3: 350}
 }
+
+var transition_duration = 200;
 
 function init() {
     d3.select(".header")
@@ -41,10 +43,170 @@ function init() {
         .style("top", svg_position.row1.height + svg_position.row2.height)
         .style("left", svg_position.row3.width1 + svg_position.row3.width2);
 
-    $.post("/pc", {},
-        function(data_infunc){
-            draw_pc(data_infunc);
+    $.post("/pc", {}, draw_pc);
+    $.post("/piechart", {}, draw_piechart);
+    $.post("/hori_bc", {}, draw_hori_bc);
+}
+
+function draw_hori_bc(dt){
+
+    var margin = {top: 25, right: 25, bottom: 25, left: 25},
+    width = svg_position.row3.width1 - margin.left - margin.right,
+    height = svg_position.row3.height - margin.top - margin.bottom;
+
+    var svg = d3.select(".horizontalbar")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var svg2 = svg.append("g");
+    var svg1 = svg.append("g");
+
+    var data_hbc = dt['data_dict'];
+    var l = data_hbc.length;
+    console.log(data_hbc);
+    var barwidth = height/l;
+
+    var x = d3.scale.ordinal().rangePoints([height,0]);
+    var y = d3.scale.linear().domain([0, d3.max(data_hbc, function(d){return d[1]+d[2]})]).range([0, width]);
+
+    var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("left");
+
+    var bar1 = svg1.selectAll("g")
+            .data(data_hbc)
+            .enter().append("g")
+            .attr("transform", function(d, i) { return "translate(" + (margin.left/2)+ ","+ (i * barwidth + margin.top/5) +")" ;})
+    var bar2 = svg2.selectAll("g")
+            .data(data_hbc)
+            .enter().append("g")
+            .attr("transform", function(d, i) { return "translate(" + (margin.left/2 + y(d[1])) + "," + (i * barwidth + margin.top/5) +")" ;})
+    
+    bar2.append("rect")
+          .attr("height", barwidth)
+          .attr("width", function(d){return (y(d[2]));})
+          .attr("fill","#3A8399");
+    bar1.append("rect")
+          .attr("height", barwidth)
+          .attr("width", function(d){return (y(d[1]));})
+          .attr("fill","orange");
+
+    bar1.append("text")
+          .attr("x", (barwidth) / 2)
+          .attr("font-size",barwidth+"px")
+          .attr("transform", function(d,i){return "translate("+ (0) +","+(barwidth/2 + 3)+")" ;})
+          .attr("fill","#B63617")
+          .text(function(d) { return d[0];});
+
+    svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate("+(margin.left/2)+"," + (margin.top/5)  + ")")
+            .call(xAxis);
+}
+
+function draw_piechart(dt) {
+    var margin = {top: 50, right: 50, bottom: 50, left: 50},
+    width = svg_position.row2.width2 - margin.left - margin.right,
+    height = svg_position.row2.height - margin.top - margin.bottom;
+    var radius = {r1: 50, r2: 100, r3: 150};
+
+    var colorCode = {
+        'White': '#82E0AA', 
+        'Black': '#F1948A', 
+        'Asian-Pac-Islander': '#f9dba0',
+        'Other': '#633974',
+        'Amer-Indian-Eskimo': '#2ca25f',
+        '<50K': 'orange',
+        '>=50K': '#3A8399'
+    }
+
+    var data = []
+    var max_val = 0
+    var margin_add = 0.5
+    for (var i = 0; i < dt.data_dict.length; i++) {
+        if (i==0) {
+            data.push([dt.data_dict[i][0], margin_add, dt.data_dict[i][1] + dt.data_dict[i][2], 'inner', dt.data_dict[i][1] + dt.data_dict[i][2]]);
+            data.push([dt.data_dict[i][0] + " <50K", margin_add, dt.data_dict[i][1], 'outer', dt.data_dict[i][1]]);
+            data.push([dt.data_dict[i][0] + " >=50K", dt.data_dict[i][1] + margin_add, dt.data_dict[i][1] + dt.data_dict[i][2], 'outer', dt.data_dict[i][2]]);
+            max_val = dt.data_dict[i][1] + dt.data_dict[i][2];
+        } else {
+            data.push([dt.data_dict[i][0], max_val + margin_add, max_val + dt.data_dict[i][1] + dt.data_dict[i][2], 'inner', dt.data_dict[i][1] + dt.data_dict[i][2]]);
+            data.push([dt.data_dict[i][0] + " <50K", max_val + margin_add, max_val + dt.data_dict[i][1], 'outer', dt.data_dict[i][1]]);
+            data.push([dt.data_dict[i][0] + " >=50K", max_val + dt.data_dict[i][1] + margin_add, max_val + dt.data_dict[i][1] + dt.data_dict[i][2], 'outer', dt.data_dict[i][2]]);
+            max_val = max_val + dt.data_dict[i][1] + dt.data_dict[i][2];
+        }
+    }
+    for (var i = 0; i < data.length; i++) {
+        data[i][1] = data[i][1]/max_val;
+        data[i][2] = data[i][2]/max_val;
+    }
+    var svg = d3.select('.piechart')
+        .append("g")
+        .attr("transform", "translate(" + (margin.left + width/2) + "," + (margin.top + height/2) + ")");
+
+    svg.selectAll('path')
+        .data(data)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr("fill", function(d, i) {return colorCode[d[0]]?colorCode[d[0]]:colorCode[d[0].substr(d[0].length-5).trim()];})
+        .on("mouseover", pieMouseOver)
+        .on("mouseout", pieMouseOut);
+
+    var textPth = d3.svg.arc()({
+        startAngle: 0,
+        endAngle: 2 * Math.PI,
+        innerRadius: 0,
+        outerRadius: radius.r1 - 1
+    });
+    svg.append('path')
+        .attr('id', 'txtPie')
+        .attr('d', textPth)
+        .attr('fill', 'white');
+
+    function path(d) {
+        var arcGenerator = d3.svg.arc();
+        var ir = radius.r1;
+        var or = radius.r2;
+        if (d[3] == 'inner') {
+            ir = radius.r1;
+            or = radius.r2;
+        } else {
+            ir = radius.r2 + 1;
+            or = radius.r3;
+        }
+        var pathData = arcGenerator({
+            startAngle: 2 * Math.PI * d[1],
+            endAngle: 2 * Math.PI * d[2],
+            innerRadius: ir,
+            outerRadius: or
         });
+        return pathData
+    }
+
+    function pieMouseOver(d, i) {
+        svg.select('#txtPie')
+            .transition().duration(transition_duration*2)
+            .attr("fill", colorCode[d[0]]?colorCode[d[0]]:colorCode[d[0].substr(d[0].length-5).trim()]);
+
+        svg.selectAll('text')
+            .data((d[0]+' Count:'+d[4]+' Share:'+(Math.round((d[2]-d[1])*100))+'%').split(' '))
+            .enter()
+            .append('text')
+            .transition().duration(transition_duration*2)
+            .attr('transform', function(d, i) {return 'translate(0, '+(-10+i*10)+')'})
+            .text(function(d, i) { return d;})
+            .style('text-anchor', 'middle');
+    }
+
+    function pieMouseOut(d, i) {
+        d3.select('#txtPie')
+            .transition().duration(transition_duration*2)
+            .attr("fill", 'white');
+        svg.selectAll('text').remove()
+    }
 }
 
 function draw_pc(dt) {
@@ -92,16 +254,13 @@ function draw_pc(dt) {
     var svg = d3.select(".parallelcoord")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
-        .append("g")
+            .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var data = []
     for (var i = 0; i < dt['data_dict'].length; i++) {
         data.push({'Index': dt['data_dict'][i][0], 'Age':dt['data_dict'][i][1], 'FnlWgt':dt['data_dict'][i][2], 'CapitalGain':dt['data_dict'][i][3], 'CapitalLoss':dt['data_dict'][i][4], 'HrsPerWk':dt['data_dict'][i][5]})
     }
-
-    console.log(dt['data_dict'])
-
 
     //Create the dimensions depending on attribute "type" (number|string)
     //The x-scale calculates the position by attribute dimensions[x].name
